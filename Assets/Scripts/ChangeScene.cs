b612a488d7c365ca47b1bad0dea5b836b320a52a
ibhupesh <ibhupesh@outlook.com>
2020-11-30 14:50:43 +0530

    Done

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour //class decalartion 
+{  //openeing braces for class
+    
+    //creating various functions for shifting from once scene to another from main menu using various buttons
+    
+    public void Scan() 
+    {
+        SceneManager.LoadScene("Smartphones");
+    }
+
+    public void Welcome() 
+    {
+        SceneManager.LoadScene("Welcome");
+    }
+
+    public void quit() //to quit the application
+    {
+        Application.Quit();
+        Debug.Log("Exit Button Pressed");
+    }
+} //closing braces for class
